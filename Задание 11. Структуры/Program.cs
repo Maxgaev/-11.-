efdb23470a67cb84d9eb1a0b373a6796324bfe53
Максим Алехин <Max-crf@mail.ru>
2021-12-20 17:01:17 +0700

    Добавьте файлы проекта.

---
+++ Задание 11. Структуры/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Задание_11.Структуры
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nлинейное уравнение ax+b=0");
+            Console.WriteLine("\nвведите переменную 'a'");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nвведите переменную 'b'");
+            double b = Convert.ToDouble(Console.ReadLine());
+            //if (a == 0)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Решений нет");
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //}
+            //else if (a == 0 || b == 0)
+            //{
+            //    Console.WriteLine("x - любое число");
+            //}
+            //else
+            //{
+            //    double result = (-b) / a;
+            //    Console.WriteLine($"\nx = {String.Format("{0:0.00}", result)}");
+            //}
+
+
+            Quad quad = new Quad(a, b);
+            Console.WriteLine(quad.Root());
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+}
---
+++ Задание 11. Структуры/Quad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_11.Структуры
+{
+    struct Quad
+    {
+        double a, b;
+
+
+        public Quad(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+
+        }
+        public string Root()
+        {
+            double result = (-b) / a;
+            return $"\nx = = {String.Format("{0:0.00}", result)}";
+        }
+
+    }
+
+}
